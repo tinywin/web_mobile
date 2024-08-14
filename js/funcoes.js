17356587b557c9a5e53a4b1320048094ba70464b
tinywin <laurahenrique9@gmail.com>
2024-08-14 20:28:00 -0300

    ajustes

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,1 +1,17 @@
+// Função para exibir uma mensagem Hello, World!
+function mostrarMensagem() {
+    alert('Hello, World!');
+}
 
+// Função para exibir a soma de dois valores fixos
+function mostrarSoma() {
+    // Valores fixos
+    var valor1 = 10; // Valor 1 fixo
+    var valor2 = 20; // Valor 2 fixo
+    
+    // Calcula a soma
+    var soma = valor1 + valor2;
+    
+    // Exibe o resultado em um alerta
+    alert('A soma dos valores é: ' + soma);
+}
